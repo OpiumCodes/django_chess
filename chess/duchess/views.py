280632aed43a424cd7e898c9a204860a47ce1712
Milea Darius <aforikrulz@gmail.com>
2020-06-25 20:21:32 +0300

    Added register form

--- chess/duchess/views.py
+++ chess/duchess/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+from .forms import *
 
 # Create your views here.
 
+    
 def play(request):
     return HttpResponse("Play")
 
@@ -10,7 +14,17 @@ def login(request):
     return render(request,'accounts/login.html')
 
 def register(request):
-    return HttpResponse("register")
+    form = UserCreationForm()
+
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, 'accounts/register.html',context)
 
 def home(request):
-    return render(request,'play/home.html')
+    games = Game.objects.all()
+    return render(request,'play/home.html',{'games':games})
